7c29a1ca4f3f7ad2252e53459e90c4aa668abb81
Brian <gamerfunky78@gmail.com>
2024-11-19 14:12:41 +0300

    change routes.jsx to Routes.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppRoutes from './routes.jsx'
+import AppRoutes from './Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
