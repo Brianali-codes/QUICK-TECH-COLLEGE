24441ac8c167fba168ab6030ed2418b9a17b81c3
Brian <gamerfunky78@gmail.com>
2024-11-25 12:09:53 +0300

    add email confirmation.

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -9,7 +9,7 @@ export default function Contacts(){
             <p className='block text-transparent'>...</p>
             <div className="p-5 flex flex-col justify-center items-center gap-0">
 
-                <form action="https://formsubmit.co/quicktechcollege@gmail.com" method="POST" className='flex flex-col gap-1 justify-center items-center'>
+                <form action="https://formsubmit.co/4f8df6ee88d4c0a602d9a4140f21139a" method="POST" className='flex flex-col gap-1 justify-center items-center'>
                     <p className='Desc text-xl font-bold'>Contact Us:</p>
                     <p>You can contact us at Tel: +254712102014 or +254724382682</p>
                     <p>If you have More info Or would like to ask a question Please go to the consultation page, or use the one below.</p>
